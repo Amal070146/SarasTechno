737cb9f8f69c3a336cf7958fce4931c021976563
Amal C P <acp070146@gmail.com>
2023-07-26 23:00:22 +0530

    service section first completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Products from "./components/Products/Products";
 import Journey from "./components/Journey/Journey";
+import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
@@ -14,6 +14,7 @@ function App() {
       <Home />
       <Products />
       <Journey />
+      <Services />
       <Contact />
       <Footer />
     </div>
