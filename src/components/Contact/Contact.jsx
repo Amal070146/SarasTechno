22ccd4a15c7eee5a5d06189056b51301f6a2017b
Amal C P <acp070146@gmail.com>
2023-07-27 23:50:43 +0530

    get in touch

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./Contact.css";
+import image from "../../assets/contact-image.png";
+
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className="ourjourney-wrapper">
+      <div className="ourjourney-image-wrapper">
+        <img src={image} alt="" />
+      </div>
+      <div className="ourjourney-content">
+        <h1>The right machine on time.</h1>
+        <p>
+          This is all some random text. This is an about us section. Write about
+          the company industry. This is all some random text.
+        </p>
+        <a href="">
+          <p>Get In Touch</p>
+          
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact
