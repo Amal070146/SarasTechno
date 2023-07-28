bb3919c47ba5aa6a6762af8effa8f4f11bf52613
Amal C P <acp070146@gmail.com>
2023-07-28 13:47:33 +0530

    footer content added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,19 +5,18 @@ import image from "../../assets/contact-image.png";
 
 const Contact = () => {
   return (
-    <div className="ourjourney-wrapper">
-      <div className="ourjourney-image-wrapper">
+    <div className="ourjourney-wrapper" style={{marginTop:0}}>
+      <div className="ourjourney-image-wrapper" style={{ background: "#fff" }}>
         <img src={image} alt="" />
       </div>
-      <div className="ourjourney-content">
+      <div className="ourjourney-content" style={{ background: "#fff",color:"black" }}>
         <h1>The right machine on time.</h1>
-        <p>
+        <p className='contact-content'>
           This is all some random text. This is an about us section. Write about
           the company industry. This is all some random text.
         </p>
         <a href="">
           <p>Get In Touch</p>
-          
         </a>
       </div>
     </div>
